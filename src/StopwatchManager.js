7531f0aff4f924fbbb5c98915bc261d3d7700a42
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2020-12-17 16:44:05 -0500

    Replace double space

--- src/StopwatchManager.js
+++ src/StopwatchManager.js
@@ -1,7 +1,21 @@
 import Databse from 'better-sqlite3'
 
 export class StopwatchManager {
-    constructor(...args) {
+    constructor(location, tablename) {
+        this.location = location
+        this.tablename = tablename
+        this.db = Databse(location)
+        this.stopwatches = []
+    }
+    add(...args) {
+        args.forEach(it => {
+            this.stopwatches += it
+        })
+    }
+    log() {
+        this.db.prepare(`CREATE TABLE IF NOT EXISTS "" ("id" TEXT NOT NULL UNIQUE, "time" NUMERIC NOT NULL);`)
+    }
+    rollback() {
 
     }
 }
