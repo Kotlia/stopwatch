e5cac6cbd384101fd23b9cae37e027edef78a9fb
Shun Ueda <62182668+Kotlia@users.noreply.github.com>
2020-12-17 11:06:05 -0500

    Period

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -1,12 +1,23 @@
 import now from 'performance-now'
 
+export { StopwatchManager } from './StopwatchManager.js'
+
 export default class Stopwatch {
     constructor(name, option = null) {
         this.name = name
-        this.option = option
         this.total = 0
         let tempStart = 0
-        let tempStop = 0
+        if (option) {
+            if (option.run) {
+                Object.entries(option.run)
+                    .forEach(k => {
+                        setTimeout(() => {
+                            k[1]()
+                        }, parseFloat(k[0]))
+                    })
+            }
+        }
+
     }
     start() {
         this.tempStart = now()
